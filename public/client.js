c189d6ddcb7c2ad5cb98fb0801b22dcda88327e6
DinoWw <dinoplecko@yahoo.com>
2022-05-11 23:41:43 +0200

    socket test complete

--- public/client.js
+++ public/client.js
@@ -1,9 +1,26 @@
+let socket;
+
+
 function setup(){
 	createCanvas(700, 700);
+
+	socket = io.connect('http://localhost:3000');
+
+	socket.on('mouseNormalizedFromServer', (data) => {
+		circle(data.x*width, data.y*height, 30);
+	});
+
+	background(0);
+	noStroke();
 }
 
 
 function draw(){
-	background(0);
-	circle(mouseX, mouseY, 100);
+}
+
+function mouseDragged(){
+	circle(mouseX, mouseY, 30);
+
+	socket.emit('mouse', {'x': mouseX/width, 'y':mouseY/height});
+	//io.sockets.emmit if the client that sent it should also recieve?
 }
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const socket = require("socket.io");
 
 const app = express();
 
 const server = app.listen(3000);
 
 app.use(express.static('public'));
+
+
+const io = socket(server);
+
+io.sockets.on('connection', function(socket){
+	
+	socket.on('mouse', (data) => {
+		socket.broadcast.emit('mouseNormalizedFromServer', data);
+	});
+
+
+});
