880d4d11b342c79690f431aa039810892f1febbe
DinoWw <dinoplecko@yahoo.com>
2022-05-14 18:36:53 +0200

    undo changes

---
+++ public/canvas/client.js
@@ -0,0 +1,32 @@
+let socket;
+
+
+function setup(){
+	createCanvas(700, 700);
+
+	console.log();
+	socket = io.connect(`${window.location.origin}/canvas`);
+
+	socket.on('mouseNormalizedFromServer', (data) => {
+		fill(0);
+		circle(data.x*width, data.y*height, 30);
+	});
+
+	background(127);
+	noStroke();
+}
+
+
+function draw(){
+}
+
+function mouseDragged(){
+	fill(255);
+	circle(mouseX, mouseY, 30);
+
+	socket.emit('mouse', {'x': mouseX/width, 'y':mouseY/height});
+	//io.sockets.emmit if the client that sent it should also recieve?
+}
+
+
+//git testinioadd 7
